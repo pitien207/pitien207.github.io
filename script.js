b656ac02644d88c2e5f92db7f6345a989150836f
pitien207 <mayyyy2603@gmail.com>
2025-05-11 17:56:42 +0200

    nghich anh chut xiu ak

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.getElementById("logoImage").addEventListener("click", function () {
+  this.classList.add("clicked"); // Add the click effect
+  setTimeout(() => {
+    location.reload(); // Refresh the page after a short delay
+  }, 100); // Wait for the effect to complete before refreshing
+});
